491f8b6a8b328b7e7bf32d165b0f9447c18cf606
Anshuman Tiwari <at3108842@gmail.com>
2025-11-24 12:20:40 +0530

    password-strength-meter

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const lowerCheck = document.getElementById("lowercase");
 const upperCheck = document.getElementById("uppercase");
 const numberCheck = document.getElementById("numbers");
 const symbolCheck = document.getElementById("symbols");
+const strength=document.getElementById("str");
 
 const generateBtn = document.getElementById("generateBtn");
 
@@ -15,7 +16,7 @@ function generatePassword() {
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const numbers = "0123456789";
-    const symbol = "!@#$%^&*()_+{}[]<>?/";
+    const symbols = "!@#$%^&*()_+{}[]<>?/";
 
     let allowedChars = "";
     let password = "";
@@ -33,12 +34,55 @@ function generatePassword() {
     }
 
     for (let i = 0; i <= length; i++) {
-        Math.random() * allowedChars.length
+       let randomIndex = Math.random() * allowedChars.length
         password += allowedChars[randomIndex];
     }
 
     passwordField.value = password;
+    
+    //here l stands for checking lower case 
+    //and u for uppercase,n for number and s for symbol
+    let l=false,u=false,n=false,s=false;
+    for(let i=0;i<length;i++){
+        for(let j=0;j<26;j++){
+            if(password[i]===lowercase[j]){
+                l=true;
+            }
+        }
+        for(let j=0;j<26;j++){
+            if(password[i]===uppercase[j]){
+                u=true;
+            }
+        }
+        for(let j=0;j<10;j++){
+            if(password[i]===numbers[j]){
+                n=true;
+            }
+        }
+        for(let j=0;j<20;j++){
+            if(password[i]===symbols[j]){
+                s=true;
+            }
+        }
+    }
+    
+    if(u&&l&&n&&s&&(length>6)){
+       strength.innerHTML = "Password is strong";
+        console.log("strong")
+    }
+
+    else if((u && l && n && (length>=4))||(length>8))
+    {
+       strength.innerHTML="the password is medium"
+        console.log("medium")
+    }
+    else{
+       strength.innerHTML="the password is easy"
+        console.log("easy")
+    }
+    
 }
+
 
 generateBtn.addEventListener("click", generatePassword);
 
